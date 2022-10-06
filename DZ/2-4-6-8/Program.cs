36a70b9a61d26727afc1209628056f6526b8ba08
Ravil <karimra@mail.ru>
2022-10-06 08:09:24 +0300

    task4

--- DZ/2-4-6-8/Program.cs
+++ DZ/2-4-6-8/Program.cs
@@ -1,5 +1,6 @@
 ﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт
 //какое число большее, а какое меньшее
+/*
 Console.WriteLine("Первое число");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Второе число");
@@ -13,3 +14,28 @@ else if (b > a)
         Console.WriteLine($"{a} меньше {b}");
     }   
     else Console.WriteLine($"{a} равны {b}");
+    */
+
+    // Задача 4
+   // Напишите программу,
+   //  которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("И третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+         if (b > max)
+             {
+                max = b;
+             }
+        if  (c > max)
+             {
+                  max=c;
+             }
+Console.Write("Максимальное число = "+max);
+         
+
+
+
